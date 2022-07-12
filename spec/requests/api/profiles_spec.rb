5fb231fa7d2e958b0d670275397702bbf8f5a108
ralist <danilov@ralist.ru>
2022-07-12 17:05:28 +0300

    add swagger

--- spec/requests/api/profiles_spec.rb
+++ spec/requests/api/profiles_spec.rb
@@ -1,9 +1,32 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/profiles', type: :request do
+
+  path '/api/profiles/{id}/toggle' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    post('toggle profile') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
   path '/api/profiles' do
+
     get('list profiles') do
       response(200, 'successful') do
+
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -17,6 +40,7 @@ RSpec.describe 'api/profiles', type: :request do
 
     post('create profile') do
       response(200, 'successful') do
+
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
