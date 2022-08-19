e365c0daae93e37bc26191bf8adf29a9b5f61529
ralist <danilov@ralist.ru>
2022-08-19 13:59:03 +0300

    tech/add sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://8c01243c992740e4924b6822aa4177a1@o1367671.ingest.sentry.io/6668610'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
