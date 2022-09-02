018fe8a6a6dc4732fecf1770f267ebc24f6fe024
ralist <danilov@ralist.ru>
2022-09-02 18:34:14 +0300

    feature/listing page fix test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,11 @@ FactoryBot.create(:user_with_profiles)
   category = Category.children.sample
   author = Profile.all.sample
   FactoryBot.create(:listing, category:, author:,
-                    image_remote_url: 'https://picsum.photos/600/450',
-                    images_attributes:
-                      [
-                        { image_remote_url: 'https://picsum.photos/600/450' },
-                        { image_remote_url: 'https://picsum.photos/600/450' }
-                      ]
-                    )
+                              image_remote_url: 'https://picsum.photos/600/450',
+                              images_attributes: [
+                                { image_remote_url: 'https://picsum.photos/600/450' },
+                                { image_remote_url: 'https://picsum.photos/600/450' }
+                              ])
 rescue StandardError => e
   pp e
   next
