2ecaf827f5c66d0d208debcaa87cc90b58fca1a9
ralist <danilov@ralist.ru>
2022-09-03 15:17:09 +0300

    feature/s3

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,20 +1,26 @@
 require 'shrine'
 require 'shrine/storage/s3'
+if Rails.env.test?
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new(prefix: 'cache'), # temporary
+    store: Shrine::Storage::Memory.new(prefix: 'store') # permanent
+  }
+else
+  bucket = Rails.env.production? ? 'tiksat-prod' : 'tiksat-dev'
+  s3_options = {
+    bucket:, # required
+    region: 'us-east-1', # required
+    access_key_id: ENV.fetch('S3_ACCESS_KEY'),
+    secret_access_key: ENV.fetch('S3_SECRET_KEY'),
+    endpoint: 'https://fra1.digitaloceanspaces.com',
+    public: true
+  }
 
-bucket = Rails.env.production? ? 'tiksat-prod' : 'tiksat-dev'
-s3_options = {
-  bucket:, # required
-  region: 'us-east-1', # required
-  access_key_id: ENV.fetch('S3_ACCESS_KEY'),
-  secret_access_key: ENV.fetch('S3_SECRET_KEY'),
-  endpoint: 'https://fra1.digitaloceanspaces.com',
-  public: true
-}
-
-Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options), # temporary
-  store: Shrine::Storage::S3.new(prefix: 'store', **s3_options) # permanent
-}
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options), # temporary
+    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options) # permanent
+  }
+end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
