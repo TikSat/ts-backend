33ea86e4f6d6543f53e8a816a07c47ac7e2a3868
Alexander Danilov <danilov@ralist.ru>
2022-09-03 15:52:42 +0300

    feature/s3 (#25)
    * feature/s3

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,29 @@
 require 'shrine'
-require 'shrine/storage/file_system'
+if Rails.env.test?
+  require 'shrine/storage/memory'
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads') # permanent
-}
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new(prefix: 'cache'), # temporary
+    store: Shrine::Storage::Memory.new(prefix: 'store') # permanent
+  }
+else
+  require 'shrine/storage/s3'
+
+  bucket = Rails.env.production? ? 'tiksat-prod' : 'tiksat-dev'
+  s3_options = {
+    bucket:, # required
+    region: 'us-east-1', # required
+    access_key_id: ENV.fetch('S3_ACCESS_KEY'),
+    secret_access_key: ENV.fetch('S3_SECRET_KEY'),
+    endpoint: 'https://fra1.digitaloceanspaces.com',
+    public: true
+  }
+
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options), # temporary
+    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options) # permanent
+  }
+end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
