fe9ec0c977f8fe25987f4af719179b5abd82165a
ralist <danilov@ralist.ru>
2022-09-08 15:32:44 +0300

    refactor/refactoring

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
+ruby '3.1.2'
+
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'api_guard'
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,34 @@ puts "Custom fields seeded at #{Time.now.strftime('%H:%M:%S %:z')}"
 
 FactoryBot.create_list(:user_with_profiles, 10)
 puts "Profiles seeded at #{Time.now.strftime('%H:%M:%S %:z')}"
+fibers = []
 
-1000.times do |i|
-  category = Category.children.sample
-  author = Profile.all.sample
-  FactoryBot.create(:listing, category:, author:,
-                              image_remote_url: 'https://picsum.photos/600/450',
-                              images_attributes: [
-                                { image_remote_url: 'https://picsum.photos/600/450' },
-                                { image_remote_url: 'https://picsum.photos/600/450' }
-                              ])
-  puts "Listing ##{i} of 1000 created"
+(ENV['RAILS_MAX_THREADS'] || 5).to_i.times do |thr_num|
+  fibers << Thread.new do
+    5.times do |i|
+      category = Category.children.sample
+      author = Profile.all.sample
+      FactoryBot.create(:listing, category:, author:,
+                                  image_remote_url: 'https://picsum.photos/600/450',
+                                  images_attributes: [
+                                    { image_remote_url: 'https://picsum.photos/600/450' },
+                                    { image_remote_url: 'https://picsum.photos/600/450' }
+                                  ])
+      puts "Listing ##{i} of 1000 created in thread #{thr_num}"
+    rescue StandardError => e
+      pp e
+      next
+    end
+  end
+end
+
+fibers.each do |f|
+  f.join
 rescue StandardError => e
   pp e
-  next
 end
 
 time_end = Time.now
 diff = (time_end - time_start)
 
-puts "DB seeded in #{diff} s."
+puts "DB seeded in #{diff} s. Listings created: #{Listing.count}"
